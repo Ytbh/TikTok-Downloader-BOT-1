3774093771f4fe2a0f7d0b9c052e2860f177d680
Ytbh <89383662+Ytbh@users.noreply.github.com>
2022-01-07 07:27:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=w
 
 @app.on_message(filters.command('start'))
 def start(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/OshadhaVimukthiM/TikTok-Downloader-BOT")]]
+    kb = [[InlineKeyboardButton('Join channel gw', url="https://t.me/projectXDF"),InlineKeyboardButton('Repo 🔰', url="https://github.com/Ytbh/TikTok-Downloader-BOT")]]
     reply_markup = InlineKeyboardMarkup(kb)
-    app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download TikTok video without Watermark.\n\n"
-                          "__**Developer :**__ __@OshadhaPMbot__\n"
-                          "__**Language :**__ __Python__\n"
-                          "__**Framework :**__ __🔥 Pyrogram__",
+    app.send_message(chat_id=message.from_user.id, text=f"Alo!, gw **Xdf57 bot**.\nGw bisa download video tiktok no wm!.\n\n"
+                          "__**Yang bikin :**__ __@YugoslavBoy__\n"
+                          "__**Bahasa :**__ __🦆Python__\n"
+                          "__**Framework :**__ __🗿 Pyrogram__",
                      parse_mode='md',
                      reply_markup=reply_markup)
 
@@ -40,10 +40,10 @@ def start(client, message):
 
 @app.on_message(filters.command('help'))
 def help(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=chnnl),InlineKeyboardButton('Repo 🔰', url="https://github.com/OshadhaVimukthiM/TikTok-Downloader-BOT")]]
+    kb = [[InlineKeyboardButton('Join channel gw', url="https://t.me/projectXDF"),InlineKeyboardButton('Repo 🔰', url="https://github.com/Ytbh/TikTok-Downloader-BOT")]]
     reply_markup = InlineKeyboardMarkup(kb)
-    app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download any TikTok video from a given link.\n\n"
-                                            "__Send me a TikTok video link__",
+    app.send_message(chat_id=message.from_user.id, text=f"Alo!, Gw **Xdf57 Bot**.\nGw bisa download video tiktok no wm tinggal kirim link .\n\n"
+                                            "__Kirimin gw link nya__",
                      parse_mode='md',
                      reply_markup=reply_markup)
 
